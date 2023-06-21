ad302296f7f44162bba63cc8375b7c83a57216fc
PandaKris <kristantosean@gmail.com>
2023-06-21 08:59:17 +0700

    Initial Commit

---
+++ CisTestVersionControl/CisTestVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CisTestVersionControlApp.swift
+//  CisTestVersionControl
+//
+//  Created by Kristanto Sean on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CisTestVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
